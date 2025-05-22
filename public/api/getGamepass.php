ac2b53cc1787b69e81e381a4b378bc685fd77fbf
rain <nematoda.tampan@gmail.com>
2025-05-22 09:50:55 -0500

    MU KING ANJING

--- public/api/getGamepass.php
+++ public/api/getGamepass.php
@@ -6,7 +6,7 @@ $price = $_GET['price'];
 $getGame = json_decode(file_get_contents("https://games.roblox.com/v1/games/$universeId/game-passes?limit=100&sortOrder=Asc"));
 $arr = [];
 foreach ($getGame->data as $i => $data) {
-    if($data->price == $price AND $data->isOwned == true) {
+    if($data->price == $price AND $data->isOwned == false) {
         $getThumbnails = json_decode(file_get_contents("https://thumbnails.roblox.com/v1/game-passes?gamePassIds=$data->id&size=150x150&format=Png&isCircular=false"), true);
         $getForSale = json_decode(file_get_contents("https://apis.roblox.com/game-passes/v1/game-passes/$data->id/product-info"), true);
         $arr[$i]  = array([
